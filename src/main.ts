82c0a55332bbc832017e3c0005fdba658d749b81
Yeongsin-ro <seungwon2093@gmail.com>
2022-07-10 12:43:19 +0900

    #33-Fix 결제수정중

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 // 오홍 깃 너무 어려워
 import { NestFactory } from '@nestjs/core';
+import { graphqlUploadExpress } from 'graphql-upload';
 //배포할때
 import { AppModule } from './app.module';
 //로컬로테스트 진행시
@@ -8,6 +9,8 @@ import { AppModuleTest } from './app.moduletest';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModuleTest);
+  // 이미지 업로드 하는데 사용 하기 위해 넣음
+  app.use(graphqlUploadExpress())
   await app.listen(3000);
 }
 bootstrap();
