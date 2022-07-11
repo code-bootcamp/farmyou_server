726c15f3253dba6a0273d1d0dc33884932198448
pukkuKim <goldie.kim89@gmail.com>
2022-07-11 14:22:16 +0900

    #33-Feat 회원정보수정

--- src/apis/user/user.resolver.ts
+++ src/apis/user/user.resolver.ts
@@ -8,10 +8,17 @@ import { GqlAuthAccessGuard } from 'src/commons/auth/gql-auth.guard';
 import { CurrentUser, ICurrentUser } from 'src/commons/auth/gql-user.param';
 import { UpdateUserInput } from './dto/updateUser.input';
 import { UpdateAddressUserInput } from '../addressUser/dto/updateAddressUser.input';
+import { AddressUser } from '../addressUser/entities/addressUser.entity';
+import { CreateAddressUserInput } from '../addressUser/dto/createAddressUser.input';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 
 @Resolver()
 export class UserResolver {
   constructor(
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
+
     private readonly userService: UserService, //
   ) {}
 
@@ -22,28 +29,32 @@ export class UserResolver {
     @Args('name') name: string,
     @Args('password') password: string,
     @Args('phone') phone: string,
+    @Args('addressUser') addressUser: CreateAddressUserInput,
+    @Args('isSeller') isSeller: boolean
   ) {
     const hashedPassword = await bcrypt.hash(password, 10.2);
     // console.log(hashedPassword);
-    return this.userService.create({ email, name, hashedPassword, phone });
+    return this.userService.create({ email, name, hashedPassword, phone, addressUser, isSeller });
   }
 
   // 회원 정보 업데이트 하기 
-  // @UseGuards(GqlAuthAccessGuard)
-  // @Mutation(() => User)
-  // async updateUser(
-  //   // @Args({name: 'email', nullable: true}) email: string,
-  //   // @Args({name: 'password', nullable: true}) password: string,
-  //   // @Args({name: 'phone', nullable: true}) phone: string,
-  //   @Args('updateUserInput') updateUserInput: UpdateUserInput,
-  //   @Args('updateAddressUserInput') updateAddressUserInput: UpdateAddressUserInput,
-  //   // @Args('updateUserInput') updateUserInput: UpdateUserInput,
+  @UseGuards(GqlAuthAccessGuard)
+  @Mutation(() => User)
+  async updateUser(
+    @CurrentUser() currentUser: ICurrentUser,
+    @Args({name: 'email', nullable: true}) email: string,
+    @Args({name: 'password', nullable: true}) password: string,
+    @Args({name: 'phone', nullable: true}) phone: string,
+    @Args({name: 'newAddress', nullable: true}) newAddress: UpdateAddressUserInput
+    // @Args('updateUserInput') updateUserInput: UpdateUserInput,
+    // @Args('updateAddressUserInput') updateAddressUserInput: UpdateAddressUserInput,
+    // @Args('updateUserInput') updateUserInput: UpdateUserInput,
 
-  //   // @Args('password') password: string,
-  // ) {
-  //   const hashedPassword = await bcrypt.hash(updateUserInput.password, 10);
-  //   return await this.userService.update({ updateUserInput, updateAddressUserInput });
-  // }
+    // @Args('password') password: string,
+  ) {
+    // const hashedPassword = await bcrypt.hash(password, 10);
+    return await this.userService.update({ currentUser, email, password, phone, newAddress });
+  }
 
   // @UseGuards(GqlAuthAccessGuard)
   // @Query(() => User)
@@ -61,8 +72,30 @@ export class UserResolver {
 
   @UseGuards(GqlAuthAccessGuard)
   @Query(() => User)
-  fetchUserLoggedIn(@CurrentUser() currentUser: ICurrentUser) {
+  fetchUserLoggedIn(
+    @CurrentUser() currentUser: ICurrentUser
+  ) {
     return this.userService.findLoggedIn({ currentUser });
+  }
+
+  @UseGuards(GqlAuthAccessGuard)
+  @Query(() => Boolean)
+  async checkIfLogged(
+    @CurrentUser() currentUser: ICurrentUser,
+    @Args('password') password: string
+  ) {
+    // const correctPassword = currentUser.password;
+    // console.log(currentUser);
+    const passwordOwner = await this.userRepository.findOne({id: currentUser.id});
+    const correctPassword = passwordOwner.password;
+
+    // console.log(passwordOwner);
+    // console.log(correctPassword);
+    // console.log(await bcrypt.hash(password, 10));
+
+    const same = bcrypt.compare(password, correctPassword);
+
+    return same;
   }
 
   // @UseGuards(GqlAuthAccessGuard)
@@ -83,17 +116,17 @@ export class UserResolver {
   //   return this.userService.updateProfile({ profile, currentUser });
   // }
 
-  @UseGuards(GqlAuthAccessGuard)
-  @Mutation(() => User)
-  async updateUser(
-    @CurrentUser() currentUser: ICurrentUser,
-    @Args('updateUserInput') updateUserInput: UpdateUserInput,
-    @Args('updateAddressUserInput') updateAddressUserInput: UpdateAddressUserInput,
-  ) {
-    return await this.userService.update({
-      email: currentUser.email,
-      updateUserInput,
-      updateAddressUserInput
-    });
-  }
+  // @UseGuards(GqlAuthAccessGuard)
+  // @Mutation(() => User)
+  // async updateUser(
+  //   @CurrentUser() currentUser: ICurrentUser,
+  //   @Args('updateUserInput') updateUserInput: UpdateUserInput,
+  //   @Args('updateAddressUserInput') updateAddressUserInput: UpdateAddressUserInput,
+  // ) {
+  //   return await this.userService.update({
+  //     email: currentUser.email,
+  //     updateUserInput,
+  //     updateAddressUserInput
+  //   });
+  // }
 }
