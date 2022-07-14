327595ece3dd489899f35d3c1a1f0ee730863eea
pukkuKim <goldie.kim89@gmail.com>
2022-07-15 05:39:48 +0900

    #15-Feat 문의 완성

--- src/apis/inquiry/inquiry.resolver.ts
+++ src/apis/inquiry/inquiry.resolver.ts
@@ -4,7 +4,6 @@ import { Inquiry } from './entities/inquiry.entity';
 import { CurrentUser, ICurrentUser } from 'src/commons/auth/gql-user.param';
 import { UseGuards } from '@nestjs/common';
 import { GqlAuthAccessGuard } from 'src/commons/auth/gql-auth.guard';
-// import { CreateInquiryInput } from './dto/createInquiry.input';
 
 @Resolver()
 export class InquiryResolver {
@@ -26,56 +25,61 @@ export class InquiryResolver {
     createInquiry(
         @Args('title') title: string,
         @Args('question') question: string,
-        // @Args('userId') userId: string,
-        // TODO: 아래 두 개 nullable 적용?
-        @Args({name: 'productDirectId', nullable: true}) productDirectId: string,
-        @Args({name: 'productUglyId', nullable: true}) productUglyId: string,
-        @CurrentUser() currentUser: ICurrentUser
+        @Args({ name: 'productDirectId', nullable: true })
+        productDirectId: string,
+        @Args({ name: 'productUglyId', nullable: true }) productUglyId: string,
+        @CurrentUser() currentUser: ICurrentUser,
     ) {
         return this.inquiryService.create(
             title,
             question,
-            // userId,
             productDirectId,
             productUglyId,
-            currentUser
+            currentUser,
         );
     }
 
     @UseGuards(GqlAuthAccessGuard)
     @Mutation(() => String)
-    // updateInquiry(
-    //     @Args('title') title: string,
-    //     @Args('question') question: string,
-    //     // @Args('userId') userId: string,
-    //     // TODO: 아래 두 개 nullable 적용?
-    //     @Args({name: 'productDirectId', nullable: true}) productDirectId: string,
-    //     @Args({name: 'productUglyId', nullable: true}) productUglyId: string,
-    //     @CurrentUser() currentUser: ICurrentUser
-    // ) {
-    //     return this.inquiryService.create(
-    //         title,
-    //         question,
-    //         // userId,
-    //         productDirectId,
-    //         productUglyId,
-    //         currentUser
-    //     );
-    // }
-
-    @UseGuards(GqlAuthAccessGuard)
-    @Mutation(() => String)
-    postResponse (
+    postResponse(
         @Args('inquiryId') inquiryId: string,
         @Args('answerTitle') answerTitle: string,
         @Args('answer') answer: string,
-        @CurrentUser() currentUser: ICurrentUser
+        @CurrentUser() currentUser: ICurrentUser,
     ) {
         return this.inquiryService.answer({
             inquiryId,
             answerTitle,
             answer,
-            currentUser
+            currentUser,
+        });
+    }
+
+    @UseGuards(GqlAuthAccessGuard)
+    @Mutation(() => Inquiry)
+    editInquiry(
+        @Args('inquiryId') inquiryId: string,
+        @Args('title') title: string,
+        @Args('question') question: string,
+        @CurrentUser() currentUser: ICurrentUser,
+    ) {
+        return this.inquiryService.edit({
+            inquiryId,
+            title,
+            question,
+            currentUser,
+        });
+    }
+
+    @UseGuards(GqlAuthAccessGuard)
+    @Mutation(() => String)
+    deleteInquiry(
+        @Args('inquiryId') inquiryId: string,
+        @CurrentUser() currentUser: ICurrentUser,
+    ) {
+        return this.inquiryService.delete({
+            inquiryId,
+            currentUser,
         });
     }
 }
