ac6ee16bd4691f643d4f8193476f7be4498ffd71
Yeongsin-ro <seungwon2093@gmail.com>
2022-07-14 12:32:26 +0900

    7월 14일 배포

--- src/apis/auth/auth.service.ts
+++ src/apis/auth/auth.service.ts
@@ -42,28 +42,28 @@ export class AuthService {
     );
   }
   
-  //소셜 로그인
-  async loginSocial(req, res) {
-    let user = await this.userService.findOne({ email: req.user.email });
+  // //소셜 로그인
+  // async loginSocial(req, res) {
+  //   let user = await this.userService.findOne({ email: req.user.email });
 
-    const hashedPassword = await bcrypt.hash(req.user.password, 10); // 비밀번호 숨겨서 보내기
-    // 2. 회원가입
-    if (!user) {
-      user = await this.userService.create({
-        email: req.user.email,
-        hashedPassword,
-        name: req.user.name,
-        phone: req.user.phone,
-        addressUser: null
-      });
-    }
+  //   const hashedPassword = await bcrypt.hash(req.user.password, 10); // 비밀번호 숨겨서 보내기
+  //   // 2. 회원가입
+  //   if (!user) {
+  //     user = await this.userService.create({
+  //       email: req.user.email,
+  //       hashedPassword,
+  //       name: req.user.name,
+  //       phone: req.user.phone,
+  //       addressUser: null
+  //     });
+  //   }
 
-    getAccessToken({ user }) {
-        return this.jwtService.sign(
-            { email: user.email, sub: user.id },
-            { secret: 'myAccessKey', expiresIn: '1h' },
-        );
-    }
+  //   getAccessToken({ user }) {
+  //       return this.jwtService.sign(
+  //           { email: user.email, sub: user.id },
+  //           { secret: 'myAccessKey', expiresIn: '1h' },
+  //       );
+  //   }
 
     //소셜 로그인
     async loginSocial(req, res) {
@@ -88,3 +88,4 @@ export class AuthService {
         res.redirect('http://localhost:5500/frontTest/social-login.test.html');
     }
 }
+
