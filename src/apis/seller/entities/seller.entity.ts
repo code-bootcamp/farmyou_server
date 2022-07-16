ffd8c9bded823eec95cc8069645c8a82f9559474
pukkuKim <goldie.kim89@gmail.com>
2022-07-17 01:13:33 +0900

    #15-Update PR 받기 전에 push

--- src/apis/seller/entities/seller.entity.ts
+++ src/apis/seller/entities/seller.entity.ts
@@ -1,57 +1,66 @@
 import { Field, Int, ObjectType } from '@nestjs/graphql';
 import { User } from 'src/apis/user/entities/user.entity';
-import { Column, Entity, PrimaryGeneratedColumn, CreateDateColumn, ManyToOne, OneToMany, JoinTable, ManyToMany } from 'typeorm';
+import {
+    Column,
+    Entity,
+    PrimaryGeneratedColumn,
+    CreateDateColumn,
+    ManyToOne,
+    OneToMany,
+    JoinTable,
+    ManyToMany,
+} from 'typeorm';
 
 @Entity()
 @ObjectType()
 export class Seller {
-  // 회원ID
-  @PrimaryGeneratedColumn('uuid')
-  @Field(() => String)
-  id: string;
+    // 회원ID
+    @PrimaryGeneratedColumn('uuid')
+    @Field(() => String)
+    id: string;
 
-  // 이름
-  @Column()
-  @Field(() => String)
-  name: string;
+    // 이름
+    @Column()
+    @Field(() => String)
+    name: string;
 
-  // 이메일
-  @Column()
-  @Field(() => String)
-  email: string;
+    // 이메일
+    @Column()
+    @Field(() => String)
+    email: string;
 
-  // 비밀번호
-  @Column({select: false})
-  // @Field(() => String) 비밀번호 노출 금지!!
-  password: string;
+    // 비밀번호
+    @Column()
+    // @Field(() => String) 비밀번호 노출 금지!!
+    password: string;
 
-  // 전화번호
-  @Column()
-  @Field(() => String)
-  phone: string;
+    // 전화번호
+    @Column()
+    @Field(() => String)
+    phone: string;
 
-  // 판매자등급
-  @Column({ default: "일반셀러" })
-  @Field(() => String)
-  grade: string;
+    // 판매자등급
+    @Column({ default: '일반셀러' })
+    @Field(() => String)
+    grade: string;
 
-  // 좋아유
-  @Column()
-  @Field(() => Number)
-  like: number
+    // 좋아유
+    @Column()
+    @Field(() => Number)
+    like: number;
 
-  // 계정타입
-  @Column({default: "seller"})
-  @Field(() => String)
-  type: string;
+    // 계정타입
+    @Column({ default: 'seller' })
+    @Field(() => String)
+    type: string;
 
-  // 회원등록날짜
-  @CreateDateColumn()
-  @Field(() => Date)
-  createdAt: Date;
+    // 회원등록날짜
+    @CreateDateColumn()
+    @Field(() => Date)
+    createdAt: Date;
 
-  @JoinTable()
-  @ManyToMany(() => User, (users) => users.sellers)
-  @Field(() => [User], {nullable: true})
-  users: User[];
+    @JoinTable()
+    @ManyToMany(() => User, (users) => users.sellers)
+    @Field(() => [User], { nullable: true })
+    users: User[];
 }
--- src/apis/user/entities/user.entity.ts
+++ src/apis/user/entities/user.entity.ts
@@ -1,53 +1,59 @@
 import { Field, ObjectType } from '@nestjs/graphql';
 import { ProductDirect } from 'src/apis/productDirect/entities/productDirect.entity';
 import { Seller } from 'src/apis/seller/entities/seller.entity';
-import { Column, Entity, PrimaryGeneratedColumn, CreateDateColumn, JoinTable, ManyToMany } from 'typeorm';
-
+import {
+    Column,
+    Entity,
+    PrimaryGeneratedColumn,
+    CreateDateColumn,
+    JoinTable,
+    ManyToMany,
+} from 'typeorm';
 
 @Entity()
 @ObjectType()
 export class User {
-  // 회원ID
-  @PrimaryGeneratedColumn('uuid')
-  @Field(() => String)
-  id: string;
+    // 회원ID
+    @PrimaryGeneratedColumn('uuid')
+    @Field(() => String)
+    id: string;
 
-  // 이름
-  @Column()
-  @Field(() => String)
-  name: string;
+    // 이름
+    @Column()
+    @Field(() => String)
+    name: string;
 
-  // 이메일
-  @Column()
-  @Field(() => String)
-  email: string;
+    // 이메일
+    @Column()
+    @Field(() => String)
+    email: string;
 
-  // 비밀번호
-  @Column({select: false})
-  // @Field(() => String) 비밀번호 노출 금지!!
-  password: string;
+    // 비밀번호
+    @Column()
+    // @Field(() => String) 비밀번호 노출 금지!!
+    password: string;
 
-  // 연락처
-  @Column()
-  @Field(() => String)
-  phone: string;
+    // 연락처
+    @Column()
+    @Field(() => String)
+    phone: string;
 
-  // 계정타입
-  @Column({default: "user"})
-  @Field(() => String)
-  type: string;
+    // 계정타입
+    @Column({ default: 'user' })
+    @Field(() => String)
+    type: string;
 
-  // 회원등록날짜
-  @CreateDateColumn()
-  @Field(() => Date)
-  createdAt: Date;
+    // 회원등록날짜
+    @CreateDateColumn()
+    @Field(() => Date)
+    createdAt: Date;
 
-//   @JoinTable()
-  @ManyToMany(() => Seller, (sellers) => sellers.users)
-  @Field(() => [Seller], {nullable: true})
-  sellers: Seller[];
+    //   @JoinTable()
+    @ManyToMany(() => Seller, (sellers) => sellers.users)
+    @Field(() => [Seller], { nullable: true })
+    sellers: Seller[];
 
-  @ManyToMany(() => ProductDirect, (directProducts) => directProducts.users)
-  @Field(() => [ProductDirect], {nullable: true})
-  directProducts: ProductDirect[];
+    @ManyToMany(() => ProductDirect, (directProducts) => directProducts.users)
+    @Field(() => [ProductDirect], { nullable: true })
+    directProducts: ProductDirect[];
 }
