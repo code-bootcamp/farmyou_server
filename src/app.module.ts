8887f03cef5beda401414fd831e873a57febc74c
pukkuKim <goldie.kim89@gmail.com>
2022-07-17 22:31:06 +0900

    #etc-이상한 문구 삭제

--- src/app.module.ts
+++ src/app.module.ts
@@ -54,12 +54,12 @@ import { AddressUserModule } from './apis/addressUser/addressUser.module';
     TypeOrmModule.forRoot({
       type: 'mysql',
       host: '172.30.192.13', //인스턴스 sql 비공개ip주소
-      // host: 'localhost',                  //내부에서 테스트로 돌려볼 호스트 주소
+    //   host: 'localhost',                  //내부에서 테스트로 돌려볼 호스트 주소
       port: 3306,
       username: 'root',
-    //   password: 'root',
-// 
       password: 'root',
+// 
+    //   password: '12345678',
       database: 'farmyou_server', //인스턴스 sql ID값
       // database:               // 내부에서 테스트로 돌려볼 데이터 베이스 이름
       entities: [__dirname + '/apis/**/*.entity.*'],
