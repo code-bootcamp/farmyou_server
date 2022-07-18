a4a917bc97b32c5cb014df08b263d3716cd5e63f
pukkuKim <goldie.kim89@gmail.com>
2022-07-19 01:12:02 +0900

    #83-Update 결제 생성할 때 상품정보도 등록

--- src/apis/payment/payment.resolver.ts
+++ src/apis/payment/payment.resolver.ts
@@ -1,5 +1,10 @@
-import { ConflictException, HttpException, UnprocessableEntityException, UseGuards } from '@nestjs/common';
-import { Mutation, Resolver, Query, Args } from '@nestjs/graphql';
+import {
+    ConflictException,
+    HttpException,
+    UnprocessableEntityException,
+    UseGuards,
+} from '@nestjs/common';
+import { Mutation, Resolver, Query, Args, registerEnumType } from '@nestjs/graphql';
 import { InjectRepository } from '@nestjs/typeorm';
 import { GqlAuthAccessGuard } from 'src/commons/auth/gql-auth.guard';
 import { CurrentUser, ICurrentUser } from 'src/commons/auth/gql-user.param';
@@ -8,58 +13,85 @@ import { IamportService } from '../iamport/iamport.service';
 import { Payment } from './entities/payment.entity';
 import { PaymentService } from './payment.service';
 import { response } from 'express';
+import { UserService } from '../user/user.service';
+import { User } from '../user/entities/user.entity';
+
+export enum PRODUCT_TYPE_ENUM {
+    UGLY_PRODUCT = 'UGLY_PRODUCT',
+    DIRECT_PRODUCT = 'DIRECT_PRODUCT',
+}
+
+registerEnumType(PRODUCT_TYPE_ENUM, {
+    name: 'PRODUCT_TYPE_ENUM',
+});
 
 @Resolver()
 export class PaymentResolver {
-  constructor(
-    private readonly paymentService: PaymentService,
-    private readonly iamportService: IamportService,
-  ) {}
-  @UseGuards(GqlAuthAccessGuard)
-  @Mutation(() => Payment)
-  async createPayment(
-    @Args("impUid") impUid: string,
-    @Args("amount") amount: number,
-    @CurrentUser() currentUser: ICurrentUser,
-  ) { 
-    // 검증로직
-    // 1. iamport 에 요청해서 결제 완료 기록이 존재하는지 확인필요
-    const token = await this.iamportService.getToken()
-    await this.iamportService.checkPaid({impUid, token, amount});
+    constructor(
+        private readonly paymentService: PaymentService,
+        private readonly iamportService: IamportService,
+
+        private readonly userService: UserService
+    ) {}
+
+    @UseGuards(GqlAuthAccessGuard)
+    @Mutation(() => Payment)
+    async createPayment(
+        @Args('impUid') impUid: string,
+        @Args('amount') amount: number,
+        @Args('productType') productType: string,
+        @Args('productId') productId: string,
+        @Args('quantity') quantity: number,
+        @CurrentUser() currentUser: ICurrentUser,
+    ) {
+        // 검증로직
+        // 1. iamport 에 요청해서 결제 완료 기록이 존재하는지 확인필요
+        const token = await this.iamportService.getToken();
+        await this.iamportService.checkPaid({ impUid, token, amount });
+
+        // 2. payment 테이블에는 impUid가 1번만 존재해야 함. (중복 결제 체크)
+        await this.paymentService.checkDuplicate({ impUid });
+
+        const theUser: User = await this.userService.buy({productType, productId, quantity, currentUser});
+
+        return this.paymentService.create({ impUid, amount, currentUser, productType, productId });
+    }
+
+    @UseGuards(GqlAuthAccessGuard)
+    @Mutation(() => Payment)
+    async cancelPayment(
+        @Args('impUid') impUid: string,
+        @CurrentUser() currentUser: ICurrentUser,
+    ) {
+        // 취소하기전 검증로직
+        // 1. 이미 취소된 건인지 확인
+        await this.paymentService.checkCanceled({ impUid });
+
+        // 2. 본인의 결제건이 맞는지 체크
+        await this.paymentService.checkUserPayment({
+            impUid,
+            currentUser,
+        });
+
+        // 3. 실제로 import 에 취소 요청하기
+        // iamport.service에서 cancel 실행
+        const token = await this.iamportService.getToken();
+        const canceledAmount = await this.iamportService.cancel({
+            impUid,
+            token,
+        });
+
+        // 4. payment 테이블에 결제 취소등록하기
+        // payment.service에서 cancel 실행
+        return await this.paymentService.cancel({
+            impUid,
+            amount: canceledAmount,
+            currentUser,
+        });
+    }
     
-    // 2. payment 테이블에는 impUid가 1번만 존재해야 함. (중복 결제 체크)
-    await this.paymentService.checkDuplicate({impUid});
-
-    return this.paymentService.create({impUid, amount, currentUser,});
-  }
-
-  @UseGuards(GqlAuthAccessGuard)
-  @Mutation(()=>Payment)
-  async cancelPayment(
-    @Args('impUid') impUid: string,
-    @CurrentUser() currentUser: ICurrentUser,
-  ) {
-    // 취소하기전 검증로직
-    // 1. 이미 취소된 건인지 확인
-    await this.paymentService.checkCanceled({impUid})
-
-    // 2. 본인의 결제건이 맞는지 체크
-    await this.paymentService.checkUserPayment({
-      impUid,
-      currentUser,
-    })
-
-    // 3. 실제로 import 에 취소 요청하기
-    // iamport.service에서 cancel 실행
-    const token = await this.iamportService.getToken()
-    const canceledAmount = await this.iamportService.cancel({impUid, token})
-
-    // 4. payment 테이블에 결제 취소등록하기
-    // payment.service에서 cancel 실행
-    return await this.paymentService.cancel({impUid, amount: canceledAmount, currentUser})
-  }
-  // @Query(() => [Payment])
-  // async fetchPayments() {
-  //   return await this.paymentService.findAll();
-  // }
+    // @Query(() => [Payment])
+    // async fetchPayments() {
+    //   return await this.paymentService.findAll();
+    // }
 }
--- src/apis/payment/payment.service.ts
+++ src/apis/payment/payment.service.ts
@@ -8,6 +8,9 @@ import { Connection, Repository } from 'typeorm';
 import { User } from '../user/entities/user.entity';
 import { Payment, PAYMENT_STATUS_ENUM } from './entities/payment.entity';
 import { IamportService } from '../iamport/iamport.service';
+import { PRODUCT_TYPE_ENUM } from './payment.resolver';
+import { ProductUgly } from '../productUgly/entities/productUgly.entity';
+import { ProductDirect } from '../productDirect/entities/productDirect.entity';
 
 @Injectable()
 export class PaymentService {
@@ -19,28 +22,68 @@ export class PaymentService {
         @InjectRepository(User)
         private readonly userRepository: Repository<User>,
 
+        @InjectRepository(ProductUgly)
+        private readonly productUglyRepository: Repository<ProductUgly>,
+
+        @InjectRepository(ProductDirect)
+        private readonly productDirectRepository: Repository<ProductDirect>,
+
         private readonly connection: Connection,
     ) {}
 
-    // 중복체크 
-    async checkDuplicate({impUid}) {
-        const result = await this.paymentRepository.findOne({impUid})
-        if(result) throw new ConflictException('이미 결제된 아이디 입니다.')
+    // 중복체크
+    async checkDuplicate({ impUid }) {
+        const result = await this.paymentRepository.findOne({ impUid });
+        if (result) {
+            throw new ConflictException('이미 결제된 아이디 입니다.');
+        }
     }
 
-    async create({ impUid, amount, currentUser, paymentComplete = PAYMENT_STATUS_ENUM.PAYMENT }) {
-        // console.log('로마로마로마')
+    async create({
+        impUid,
+        amount,
+        currentUser,
+        productType,
+        productId,
+        paymentComplete = PAYMENT_STATUS_ENUM.PAYMENT,
+    }) {
         const thisUser = await this.userRepository.findOne({
             // relations: ['sellers', 'directProducts'],
-            where: {id: currentUser.id}
-          });
-        // 1. 거래기록 1줄 생성 해야함
-        const payment = this.paymentRepository.create({
-            impUid: impUid,
-            amount: amount,
-            user: thisUser,
-            paymentComplete,
+            where: { id: currentUser.id },
         });
+
+        let theProduct;
+        let payment;
+
+        if (productType === PRODUCT_TYPE_ENUM.UGLY_PRODUCT) {
+            theProduct = await this.productUglyRepository.findOne({
+                relations: ['users', 'seller'],
+                where: {id: productId}
+            });
+            // 1. 거래기록 1줄 생성 해야함
+            payment = this.paymentRepository.create({
+                impUid: impUid,
+                amount: amount,
+                user: thisUser,
+                productUgly: theProduct,
+                paymentComplete,
+            });
+        } else if (productType === PRODUCT_TYPE_ENUM.DIRECT_PRODUCT) {
+            theProduct = await this.productDirectRepository.findOne({
+                relations: ['category', 'directStore', 'users', 'admin'],
+                where: {id: productId}
+            });
+            // 1. 거래기록 1줄 생성 해야함
+            payment = this.paymentRepository.create({
+                impUid: impUid,
+                amount: amount,
+                user: thisUser,
+                productDirect: theProduct,
+                paymentComplete,
+            });
+        } else {
+            throw new UnprocessableEntityException('올바른 productType을 입력해주세요.');
+        }
         console.log(payment);
         await this.paymentRepository.save(payment);
 
@@ -60,39 +103,41 @@ export class PaymentService {
         // 4. 프론트엔드에 최종결과 돌려주기
         return payment;
     }
-    
+
     // 취소된 건인지 확인
-    async checkCanceled({impUid}){
+    async checkCanceled({ impUid }) {
         const payment = await this.paymentRepository.findOne({
             impUid,
             paymentComplete: PAYMENT_STATUS_ENUM.CANCEL,
-        })
+        });
         // 이미 취소된 건이면 오류 던지기
-        if(payment) throw new ConflictException('이미 취소된 건 입니다.')
+        if (payment) throw new ConflictException('이미 취소된 건 입니다.');
     }
 
     // 다른사람의 결제건을 환불 받지 못하게 하기 위해 자신이 결제한 건인지 체크
-    async checkUserPayment({impUid, currentUser}){
+    async checkUserPayment({ impUid, currentUser }) {
         const checkUser = await this.paymentRepository.findOne({
             impUid,
-            user: {id: currentUser.id},
+            user: { id: currentUser.id },
             paymentComplete: PAYMENT_STATUS_ENUM.PAYMENT,
-        })
+        });
         // 접속한 유저id 와 impUid 가 같지 않은 유저에게는 오류 던지기
-        if(!checkUser)
-        throw new UnprocessableEntityException('결제기록이 없습니다.')
+        if (!checkUser)
+            throw new UnprocessableEntityException('결제기록이 없습니다.');
     }
 
     // 페이먼트 테이블에서 결제 취소 데이터 등록하기
     // cancel 이란 데이터를 추가로 만드는것이고 payment의 상태를 바꾸는 것이 아님
     // 위의 create를 재활용 합니다.
-    async cancel({impUid, amount, currentUser}){
+    async cancel({ impUid, amount, currentUser }) {
         const payment = await this.create({
             impUid,
             amount: -amount,
             currentUser,
-            paymentComplete: PAYMENT_STATUS_ENUM.CANCEL
-        })
-        return payment
+            productType: null,
+            productId: null,
+            paymentComplete: PAYMENT_STATUS_ENUM.CANCEL,
+        });
+        return payment;
     }
 }
